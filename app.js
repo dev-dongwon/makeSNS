6cfcd0147afb9c32f9f5df759a08e0c3e683921b
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-12 21:48:23 +0900

    Feat : file 경로 변경

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+// module
+const express       = require('express'),
+      app           = express(),
+      createError   = require('http-errors'),
+      path          = require('path'),
+      cookieParser  = require('cookie-parser'),
+      logger        = require('morgan'),
+      bodyParser    = require('body-parser'),
+      mongoose      = require('mongoose')
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// routes
+const indexRouter   = require('./routes/index'),
+      usersRouter   = require('./routes/users'),
+      apiRouter     = require('./routes/api')
 
-const app = express();
+// DB
+mongoose.Promise = global.Promise;
+let url = process.env.DATABASE_URL || "mongodb://localhost:27017/test";
+mongoose.connect(url, { useNewUrlParser: true })
+.then(() => console.log('Successfully connected to mongodb'))
+.catch(e => console.error(e));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -19,6 +29,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api', apiRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
