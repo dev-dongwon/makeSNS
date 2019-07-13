a295892996464a03553277e212f4df6656c54ed6
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-14 02:15:15 +0900

    feat : local-login 전략 사용해 로그인 및 인증 구현

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
+
+require('../model/passport').setup()
 
 // get signIn page
 router.get('/', (req, res, next) => {
+  console.log(req.user)
   res.render('login', { title: 'LOGIN | SNS' , page : 'LOGIN'});
 });
 
-router.post('/', (req, res, next) => {
-  
-})
+// local-login Strategy
+router.post('/', passport.authenticate('local-login', {
+  successRedirect : '/',
+  failureRedirect : '/login',
+  failureFlash : true,
+  successFlash : true,
+}))
 
 module.exports = router;
