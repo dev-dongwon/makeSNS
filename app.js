294fe6db469acf7a9c5fb4f9c02301e3c9a02b3f
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-14 02:11:51 +0900

    Feat : 로그인 라우터 구현 및 모듈 임포트

--- app.js
+++ app.js
@@ -7,38 +7,60 @@ const express       = require('express'),
       logger        = require('morgan'),
       bodyParser    = require('body-parser'),
       mongoose      = require('mongoose'),
-      session       = require('express-session');
+      session       = require('express-session'),
+      RedisStore    = require('connect-redis')(session),
+      passport      = require('passport'),
+      flash         = require('connect-flash'),
 
-// routes
+// routes module
 const indexRouter   = require('./routes/index'),
       loginRouter   = require('./routes/login'),
       usersRouter   = require('./routes/users')
 
 // DB
 mongoose.Promise = global.Promise;
-let url = process.env.DATABASE_URL || "mongodb://localhost:27017/test";
-mongoose.connect(url, { useNewUrlParser: true })
-.then(() => console.log('Successfully connected to mongodb'))
-.catch(e => console.error(e));
+const url = process.env.DATABASE_URL || "mongodb://localhost:27017/test";
+mongoose.connect(url, { useNewUrlParser: true });
+const db = mongoose.connection;
 
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Conneted mongoDB');
+});
+
+// http
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// view
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(session({
   secret: 'secret',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  }
 }))
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cookieParser());
+app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 
+// error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
