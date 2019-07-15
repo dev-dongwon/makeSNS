0df3aeeb5e2adb5580cfd4f0693985e31533f82a
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-15 19:00:37 +0900

    Feat : bcrypt 모듈로 password 저장 전 암호화 기능 구현

--- model/user.js
+++ model/user.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
-  id : String,
-  email : String,
-  password : String
+  email : {
+    type : String,
+    required : true,
+    unique : true
+  },
+  password : {
+    String : String,
+    required : true
+  }
 });
+
+userSchema.pre('save', async function(next) {
+  const user = this;
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+})
 
 userSchema.methods.validPassword = function(password) {
   return password === this.password;
