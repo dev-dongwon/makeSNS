af2b851004e8f7bb508076ac69bc6aad981f61fb
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-16 20:21:31 +0900

    Feat : 회원가입 시 조건에 따라 redirect url 분기 설정
    회원가입 성공 시 로그인 페이지로,
    회원가입 실패 시 다시 회원가입 페이지로 리다이렉트

--- controller/userController.js
+++ controller/userController.js
@@ -4,12 +4,19 @@ require('../auth/passport').setup()
 
 const userController = {
 
-  addUser : async (req, res, next) => {
-    res.json({ 
-      message : 'Signup successful',
-      user : req.user 
-    });
-  }
+  addUser : (req, res, next) => {
+      passport.authenticate('signup', {
+        session : false
+      }, (err, user, info) => {
+        req.flash('INFO',info.message)
+        
+        if (err || !user) {
+          return res.redirect('/signup')
+        }
+
+        return res.redirect('/signin');
+      })(req, res, next)
+    }
 }
 
 module.exports = userController;
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const userController = require('../controller/userController');
 const router = express.Router();
-
 const passport = require('passport');
 
 require('../auth/passport').setup()
 
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-router.post('/', passport.authenticate('signup', { session : false }), async (req, res, next) => {
-  userController.addUser(res, res, next);
+router.post('/', (req, res, next) => {
+  userController.addUser(req, res, next);
 })
 
 module.exports = router;
