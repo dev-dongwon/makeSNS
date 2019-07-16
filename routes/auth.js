cfca636bdbc166da68c098d5b33b3f9b3d289451
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-16 17:26:22 +0900

    Feat : local-lpgin 라우트 추가

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const express = require('express'),
       authController = require('../controller/authController');
 
 // local-login
-router.post('/local-login', (req, res) => authController.localLogin(req, res))
+router.post('/local-login', (req, res, next) => {
+  authController.localLogin(req, res, next)
+})
 
 module.exports = router;
