11a3a2bfcd2bf406c701eaf592f770ebc1bcb287
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-16 17:26:51 +0900

    Feat : signup 페이지로 이동하는 라우트 추가

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,16 @@ const express         = require('express'),
       indexController = require('../controller/indexController');
 
 router.get('/', (req, res) => {
+  console.log(req.flash(), req.session)
   indexController.home(req, res);
 });
 
 router.get('/signin', (req, res) => {
   indexController.signin(req, res);
+})
+
+router.get('/signup', (req, res) => {
+  indexController.signup(req, res);
 })
 
 module.exports = router;
