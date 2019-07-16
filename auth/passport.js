e68034ba69483ba5075d8336f8c975ea958c538d
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-16 17:11:56 +0900

    Feat : passport signup, jwt 토큰 검증 로직 추가
    회원가입, jwt 토큰 확인하는 passport 로직  추가
    callback 패턴을 async, await으로 변경

--- auth/passport.js
+++ auth/passport.js
@@ -1,35 +1,54 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const config = require('../config/index');
 const User = require('../model/user');
 
 exports.setup = () => {
   passport.use('local-login', new LocalStrategy({
-      usernameField: 'id',
+      usernameField: 'email',
       passwordField: 'password',
-      passReqToCallback : true
-    }, (req, id, password, done) => {
-      User.findOne({
-        id: id
-      }, (err, user) => {
-        if (err) {
-          return done(err);
-        }
+    }, async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
         if (!user) {
           return done(null, false, {message : 'invaild username or password'});
         }
-        if (!user.validPassword(password)) {
+        if (!user.isValidPassword(password)) {
           return done(null, false, {message : 'invaild username or password'});
         }
-        return done(null, user, {message : `${user.id}님, 환영합니다!`});
-      })
+        return done(null, user, { message : 'Logged in Successfully'});
+      } catch (error) {
+        return done(error);
+      }
     }
   )),
 
-  passport.serializeUser(function(user, done) {
-    done(null, user);
-  });
-  
-  passport.deserializeUser(function(user, done) {
-    done(null, user);
-  });
+  passport.use('signup', new LocalStrategy({
+    usernameField : 'email',
+    passwordField : 'password',
+  }, async (email, password, done) => {
+    try {
+        const existUser = await User.findOne({ email });
+        if (existUser) {
+          return done(null, false, {message : 'member Exist'});
+        }
+        const user = await User.create({ email, password });
+        return done(null, user);
+      } catch (error) {
+        done(error);
+      }
+  })),
+
+  passport.use('jwt', new JwtStrategy({
+    secretOrKey : config.jwtSecret,
+    jwtFromRequest : ExtractJWT.fromAuthHeaderAsBearerToken()
+  }, async (token, done) => {
+    try {
+      return done(null, token.user);
+    } catch (error) {
+      done(error);
+    }
+  }))
 }
