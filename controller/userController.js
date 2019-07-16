b09e282710356e54b7d07b70cde5cdb6ef8538d7
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-16 17:23:57 +0900

    Feat : 회원가입 기능 추가
    passport에서 회원가입, 생성 후 user controller에서 이후 과정 컨트롤

--- controller/userController.js
+++ controller/userController.js
@@ -1,28 +1,14 @@
 const User = require('../model/user');
+const passport = require('passport');
+require('../auth/passport').setup()
 
 const userController = {
-  addUser : async (req, res) => {
-    const { id, email, password } = req.body;
-    
-    const create = (user) => {
-      if (user) {
-        throw new Error('username exists');
-      } else {
-        return User.create({id, password, email});
-      }
-    }
-    
-    try {
-      const user = await User.findOne({ id : id });
-      const newUser = await create(user);
-      const ss = await User.findOne({ id : id })
-      res.end('/')
-      console.log(req.user)
-    } catch (error) {
-      req.flash('message','가입할 수 없엉');
-      res.redirect('/');
-      console.error(error);
-    }
+
+  addUser : async (req, res, next) => {
+    res.json({ 
+      message : 'Signup successful',
+      user : req.user 
+    });
   }
 }
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const userController = require('../controller/userController');
 const router = express.Router();
+
+const passport = require('passport');
+
+require('../auth/passport').setup()
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/', passport.authenticate('signup', { session : false }), async (req, res, next) => {
+  userController.addUser(res, res, next);
+})
 
 module.exports = router;
