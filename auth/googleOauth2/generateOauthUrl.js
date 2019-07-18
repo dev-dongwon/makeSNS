4c877cdcfc0b4d719878138a987f9f7eb36d3f18
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-18 16:08:53 +0900

    Feat : google oauth toekn 발급을 위한 url 가져오기

---
+++ auth/googleOauth2/generateOauthUrl.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const { google } = require('googleapis');
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GMAIL_OAUTH_CLIENT_ID,
+  process.env.GMAIL_OAUTH_CLIENT_SECRET,
+  process.env.GMAIL_OAUTH_REDIRECT_URL,
+);
+
+console.log(require('dotenv').config())
+
+const GMAIL_SCOPES = [
+  'https://mail.google.com/',
+  'https://www.googleapis.com/auth/gmail.modify',
+  'https://www.googleapis.com/auth/gmail.compose',
+  'https://www.googleapis.com/auth/gmail.send',
+];
+
+const url = oauth2Client.generateAuthUrl({
+  access_type: 'offline',
+  scope: GMAIL_SCOPES,
+});
+
+console.info(`authUrl: ${url}`);
