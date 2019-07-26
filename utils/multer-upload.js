5645c3b9ae09217d4523c50d87016848a610eaa1
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-26 10:38:41 +0900

    Modified : multer S3 저장 시 파일이름 변경 후 저장

--- utils/multer-upload.js
+++ utils/multer-upload.js
@@ -13,8 +13,7 @@ const upload = multer({
       contentType: multerS3.AUTO_CONTENT_TYPE,
       acl: 'public-read',
       key: (req, file, cb) => {
-          console.log(file);
-          cb(null, file.originalname)
+        cb(null, Date.now() + '-' + file.originalname);
       },
   }),
   limits: { fileSize: 5 * 1024 * 1024 },
