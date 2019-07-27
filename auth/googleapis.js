a7c4777c6281530ef89ba64a148366723f6cb509
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-27 20:36:33 +0900

    Feat : google api 유틸 파일 작성
    google api를 활용해 토큰을 얻는 유틸

---
+++ auth/googleapis.js
@@ -0,0 +1,29 @@
+const {google} = require('googleapis');
+require('dotenv').config();
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GMAIL_OAUTH_CLIENT_ID,
+  process.env.GMAIL_OAUTH_CLIENT_SECRET,
+  process.env.GMAIL_OAUTH_REDIRECT_URL
+);
+
+const scopes = [
+  'https://www.googleapis.com/auth/plus.login',	
+  'https://www.googleapis.com/auth/plus.me',
+  'https://www.googleapis.com/auth/userinfo.email',
+]
+
+const url = oauth2Client.generateAuthUrl({
+  access_type: 'offline',
+  scope: scopes
+})
+
+const getGooglePlusApi = (auth) => {
+  return google.plus({ version: 'v1', auth });
+}
+
+module.exports = {
+  url,
+  oauth2Client,
+  getGooglePlusApi
+}
