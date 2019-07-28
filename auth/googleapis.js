fae1e5d0dc33265a1b99a10e2f5e5d8b8239f033
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-28 19:05:19 +0900

    Feat : google Oauth register 인증 정보 추가
    리다이렉트 url 수정 후 추가

--- auth/googleapis.js
+++ auth/googleapis.js
@@ -7,6 +7,13 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.GMAIL_OAUTH_REDIRECT_URL
 );
 
+
+const oauth2ClientForRegister = new google.auth.OAuth2(
+  process.env.GMAIL_OAUTH_CLIENT_ID,
+  process.env.GMAIL_OAUTH_CLIENT_SECRET,
+  process.env.GMAIL_OAUTH_REGISTER_REDIRECT_URL
+);
+
 const scopes = [
   'https://www.googleapis.com/auth/plus.login',	
   'https://www.googleapis.com/auth/plus.me',
@@ -18,6 +25,11 @@ const url = oauth2Client.generateAuthUrl({
   scope: scopes
 })
 
+const registerUrl = oauth2ClientForRegister.generateAuthUrl({
+  access_type: 'offline',
+  scope: scopes
+})
+
 const getGooglePlusApi = (auth) => {
   return google.plus({ version: 'v1', auth });
 }
@@ -25,5 +37,7 @@ const getGooglePlusApi = (auth) => {
 module.exports = {
   url,
   oauth2Client,
+  oauth2ClientForRegister,
+  registerUrl,
   getGooglePlusApi
 }
