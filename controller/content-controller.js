fcf6190457655d2f361adb75509d5d47757db79c
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-29 22:33:32 +0900

    Feat : content page를 불러오는 controller 추가
    user, content 정보를 담아 page 렌더링

--- controller/content-controller.js
+++ controller/content-controller.js
@@ -1,7 +1,14 @@
 const Post = require('../model/post');
 
 const contentController = {
-
+  getContentPage: async (req, res) => {
+    const content = await Post.findById(req.params.contentNumber);
+    res.render('content', {
+      title: 'Daily Frame | The creators Network',
+      user: req.user,
+      content
+    });
+  }
 }
 
 module.exports = contentController;
