6ac856e54ad9a3e572254c9bf078b5b768884ac5
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-30 01:06:02 +0900

    Feat : contents 조회 시 조회수 view 증가 기능 추가
    조회 시 view 증가 db에 반영

--- controller/content-controller.js
+++ controller/content-controller.js
@@ -3,6 +3,9 @@ const Post = require('../model/post');
 const contentController = {
   getContentPage: async (req, res) => {
     const content = await Post.findById(req.params.contentNumber);
+    content.meta.views += 1;
+    await content.save();
+
     res.render('content', {
       title: 'Daily Frame | The creators Network',
       user: req.user,
