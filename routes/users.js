bf13e6451ddc032a0fc9db360aa7e2faca1464fc
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-29 20:28:09 +0900

    Feat : initSettings 라우터 추가
    initSettings 페이지로 이동하는 라우터 추가

--- routes/users.js
+++ routes/users.js
@@ -8,12 +8,16 @@ router.post('/', (req, res, next) => {
   userController.addUser(req, res, next);
 })
 
-router.patch('/:username', (req, res, next) => {
+router.patch('/:usernameOrOauthId', (req, res, next) => {
   userController.updateUser(req, res, next);
 })
 
 router.get('/settings', (req, res) => {
   userController.getSettingsPage(req, res)
+})
+
+router.get('/initSettings', (req, res) => {
+  userController.getInitSettingsPage(req, res)
 })
 
 module.exports = router;
