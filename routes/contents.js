8d79526a6b09c9c00914c71fdd2ad7f48755edcf
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-30 02:31:38 +0900

    Feat : 콘텐츠 삭제를 위한 라우터  추가

--- routes/contents.js
+++ routes/contents.js
@@ -2,8 +2,12 @@ const express         = require('express'),
       router          = express.Router(),
       contentController = require('../controller/content-controller');
 
-router.get('/:contentNumber', (req, res) => {
-  contentController.getContentPage(req, res);
+router.get('/:contentNumber', (req, res, next) => {
+  contentController.getContentPage(req, res, next);
 });
+
+router.delete('/:contentNumber', (req, res, next) => {
+  contentController.deleteContent(req, res, next);
+})
 
 module.exports = router;
