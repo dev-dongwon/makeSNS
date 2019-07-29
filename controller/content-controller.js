3df824b2eeffd9be5d93e900e4a93a7be2979952
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-30 02:32:25 +0900

    Feat : 콘텐츠 삭제 컨트롤러 추가

--- controller/content-controller.js
+++ controller/content-controller.js
@@ -1,17 +1,36 @@
 const Post = require('../model/post');
 
 const contentController = {
-  getContentPage: async (req, res) => {
-    const content = await Post.findById(req.params.contentNumber);
-    content.meta.views += 1;
-    await content.save();
+  getContentPage: async (req, res, next) => {
+    try {
+      const content = await Post.findById(req.params.contentNumber);
+      content.meta.views += 1;
+      await content.save();
+  
+      res.render('content', {
+        title: 'Daily Frame | The creators Network',
+        user: req.user,
+        content
+      });
+      
+    } catch (error) {
+      next(error);
+    }
+  },
 
-    res.render('content', {
-      title: 'Daily Frame | The creators Network',
-      user: req.user,
-      content
-    });
-  }
+  deleteContent : async (req, res, next) => {
+
+    try {
+      const content = await Post.findById(req.params.contentNumber);
+      content.display = false;
+      await content.save();
+  
+      return res.end('success');
+    } catch (error) {
+      next(error);
+    }
+  },
+
 }
 
 module.exports = contentController;
