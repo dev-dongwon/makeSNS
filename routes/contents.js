e4f2438c01975b37fa566896518659576bee3265
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-30 21:30:03 +0900

    Feat : content update를 위한 라우터 생성

--- routes/contents.js
+++ routes/contents.js
@@ -1,6 +1,7 @@
 const express         = require('express'),
       router          = express.Router(),
-      contentController = require('../controller/content-controller');
+      contentController = require('../controller/content-controller'),
+      upload          = require('../utils/multer-upload');
 
 router.get('/:contentNumber', (req, res, next) => {
   contentController.getContentPage(req, res, next);
@@ -8,6 +9,10 @@ router.get('/:contentNumber', (req, res, next) => {
 
 router.delete('/:contentNumber', (req, res, next) => {
   contentController.deleteContent(req, res, next);
+})
+
+router.patch('/:contentNumber', upload.array('image', 4), (req, res, next) => {
+  contentController.updateContent(req, res, next);
 })
 
 module.exports = router;
