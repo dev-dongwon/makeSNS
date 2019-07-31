6c2153d561692c20d6a4460b8cbc2a696164c556
dev-dongwon <dev.dongwonkim@gmail.com>
2019-07-31 21:03:05 +0900

    Feat : postController에 로그인 판별 미들웨어 추가

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,10 @@
 const express         = require('express'),
       router          = express.Router(),
       postController  = require('../controller/post-controller'),
-      upload          = require('../utils/multer-upload');
+      upload          = require('../utils/multer-upload'),
+      authMiddlewares = require('../middlewares/auth')
 
-router.post('/', upload.array('image', 4), (req, res, next) => {
+router.post('/', authMiddlewares.isLoggedIn, upload.array('image', 4), (req, res, next) => {
   postController.uploadImage(req, res, next);
 })
 
