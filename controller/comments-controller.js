7aa285a70d6fd89c7d8bf4fe0f3df52215bb383f
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-01 21:08:28 +0900

    Modified : 댓글 count 증가 추가
    포스트에 댓글이 달리면 count display가 1 증가

--- controller/comments-controller.js
+++ controller/comments-controller.js
@@ -3,22 +3,26 @@ const Comment = require('../model/comment');
 
 const commentsController = {
   addComment : async (req, res, next) => {
-    const user = req.user;
-    const {comment, contentId} = req.body
-    
-    const newComment = await Comment.create({
-      content : comment,
-      authorId : user._id,
-      postId : contentId,
-      username : user.username,
-      userAvatar : user.profilePhoto,
-    })
-
-    const post = await Post.findById(contentId);
-    post.comment.push(newComment);
-    post.save();
-
-    return res.json(newComment);
+    try {
+      const user = req.user;
+      const {comment, contentId} = req.body
+      
+      const newComment = await Comment.create({
+        content : comment,
+        authorId : user._id,
+        postId : contentId,
+        username : user.username,
+        userAvatar : user.profilePhoto,
+      })
+  
+      const post = await Post.findById(contentId);
+      post.comment.push(newComment);
+      post.meta.comments += 1;
+      post.save();
+      return res.json(newComment);
+    } catch (error) {
+      next(error);
+    }
   },
 }
 
