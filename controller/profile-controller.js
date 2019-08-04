9b557a24bea133861c614e82860b916ce1c21b33
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-04 17:27:29 +0900

    Feat : post 객체를 가진 user 객체를 view로 렌더링
    _id를 가진 psot를 populate해 실제 객체로 변환

--- controller/profile-controller.js
+++ controller/profile-controller.js
@@ -1,11 +1,15 @@
 const Post = require('../model/post');
+const User = require('../model/user')
 
 const profileController = {
   home: async (req, res) => {
+    const user = await User.findOne({username : req.user.username})
+                           .populate({path : 'posts'});
 
     res.render('profile', {
       title: 'Profile | The creators Network',
-      user: req.user,
+      user: user,
+      likes : JSON.stringify(user.likePosts)
     });
   },
 }
