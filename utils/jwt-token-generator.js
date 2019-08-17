eff0bf3d80a202f2eb21e7f7c6f2ed05918c4f96
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-17 22:37:16 +0900

    Feat : JWT token generator 생성
    토큰을 생성하는 반복작업을 수행하는 유틸 생성

---
+++ utils/jwt-token-generator.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+
+const generateJWTToken = async (res, obj) => {
+  const token = await jwt.sign({ obj }, process.env.JWT_SECRET);
+  res.cookie('token', token, {
+    httpOnly : true,
+    maxAge: 1000*60*60
+  });
+  return token;
+}
+
+module.exports = generateJWTToken;
