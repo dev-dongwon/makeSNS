fa73d4f1f8f9f8d57747ad5762e3e37177f9ac58
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-18 14:42:07 +0900

    Modified : jwt payload key 값 수정
    user로 수정

--- utils/jwt-token-generator.js
+++ utils/jwt-token-generator.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 const generateJWTToken = async (res, obj) => {
-  const token = await jwt.sign({ obj }, process.env.JWT_SECRET);
+  const token = await jwt.sign({ "user": obj }, process.env.JWT_SECRET);
   res.cookie('token', token, {
     httpOnly : true,
     maxAge: 1000*60*60
