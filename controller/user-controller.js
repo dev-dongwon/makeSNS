162bb41bf32a764708d7ec7b6083a5c63e6022b6
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-18 21:05:19 +0900

    Feat : Oauth 유저 회원가입 컨트롤러 분리
    Oaath 유저 회원가입 컨트롤러 추가

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -2,9 +2,76 @@ const generateJWTToken = require('../utils/jwt-token-generator');
 const User = require('../model/user');
 const Post = require('../model/post');
 const passport = require('passport');
+const userHandler = require('../utils/db/user');
+const pool = require('../db/connect-mysql').pool;
 require('../auth/passport').setup()
 
 const userController = {
+
+  addAuthUser : async (req, res, next) => {
+    try {
+      const [rows] =  await pool.query(
+        `
+        SELECT
+          * 
+        FROM
+          USERS
+        WHERE
+          auth_google_id = "${req.body.authGoogleId}"`
+        );
+
+        console.log(rows);
+
+      if (rows.length > 0) {
+        req.flash('message', {'info' : '이미 google 회원가입이 되어 있습니다!'});
+        return res.redirect('/signin');
+      }
+
+      // 클라이언트에서 중복 체크 후 서버에서도 중복 유저 네임 체크
+      const checkDupleUsernameInfo = await pool.query(
+        `
+        SELECT
+        COUNT(*) dupleUsernameCount
+        FROM
+        USERS
+        WHERE
+        username = "${req.body.username}"
+        `)
+        
+      const [dupleUsername] = checkDupleUsernameInfo;
+      const {dupleUsernameCount} = dupleUsername[0];
+  
+      if (dupleUsernameCount > 0) {
+        return res.end('dupleUsername');
+      }
+
+      if (req.files.length > 0) {
+        req.body.photolink = req.files[0].location
+      }
+
+      const {username, location, bio, link, authGoogleId, photolink} = req.body;
+
+      const [result] = await pool.query(
+      
+      `
+        INSERT INTO USERS
+        (username, location, bio, link, auth_google_id, photo_link)
+        VALUES
+        ("${username}", "${location}", "${bio}", "${link}", "${authGoogleId}", "${photolink}");
+      `
+      )
+      
+      if (result.affectedRows === 1 && result.serverStatus === 2) {
+        // db insert 후 회원가입 완료, jwt 토큰 발급
+        const user = userHandler.makeUserObj(result.insertId, username, photolink);
+        await generateJWTToken(res, user);
+        return res.end('success');
+      }
+      
+    } catch (error) {
+      next(error);
+    }
+  },
 
   addUser : (req, res, next) => {
     try {
