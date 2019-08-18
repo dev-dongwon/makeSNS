1c6571e9fd1f268d48bcf6a6417f80d8a0d4df67
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-18 21:07:31 +0900

    Feat : Oauth 회원가입 라우터 추가
    라우터 추가 후 필요한 주석 작성

--- routes/users.js
+++ routes/users.js
@@ -6,16 +6,25 @@ const express     = require('express'),
 
       require('../auth/passport').setup()
 
+// local 회원가입
 router.post('/', userController.addUser);
 
+// Oauth 회원가입
+router.post('/auth', upload.array('image', 4), userController.addAuthUser);
+
+// user 정보 업데이트
 router.patch('/:usernameOrOauthId', upload.array('image', 4), userController.updateUser);
 
+// user setting 페이지
 router.get('/settings', auth.isLoggedIn, userController.getSettingsPage);
 
+// 최초 회원가입 후 정보 settings 페이지
 router.get('/initSettings', auth.isLoggedIn, userController.getInitSettingsPage);
 
+// '좋아요'를 누른 목록 보기 페이지
 router.get('/:username/likes', userController.getLikesPage);
 
+// 회원탈퇴를 위한 user 정보 삭제
 router.delete('/:username', auth.isLoggedIn, userController.deleteUser);
 
 module.exports = router;
