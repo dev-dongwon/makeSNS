ac9d9fcef08a948246396cc7687ab777e5869968
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-19 20:47:16 +0900

    Delete : 불필요한 disdover 라우터, 컨트롤러 삭제
    현재 쓰지 않는 좋아요 순 게시물 정렬 관련 라우터, 컨트롤러
    index 라우터나 ajax로 전환 예정

--- routes/discover.js
+++
@@ -1,7 +0,0 @@
-const express         = require('express'),
-      router          = express.Router(),
-      discoverController = require('../controller/discover-controller');
-
-router.get('/trending', discoverController.trending);
-
-module.exports = router;
