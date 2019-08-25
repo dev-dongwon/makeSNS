0bb11283dc616b81cb53ce2bbb86003b5074c00b
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-25 19:48:42 +0900

    Modifeid : profile 페이지 mysql로 변환
    controller mysql로 변환 및 스키마에 맞게 view 수정

--- controller/profile-controller.js
+++ controller/profile-controller.js
@@ -1,28 +1,82 @@
-const Post = require('../model/post');
-const User = require('../model/user')
+const pool = require("../db/connect-mysql").pool;
 
 const profileController = {
-  home: async (req, res) => {
+  home: async (req, res, next) => {
+    try {
+      let authorUsername = req.params.username;
 
-    const author = await User.findOne({username : req.params.username})
-                           .populate({path : 'posts'});
+      // 해당 profile 정보 가져오기
+      const [authorRow] = await pool.query(`
+        SELECT id, username, location, introduction, photo_link
+        FROM USERS
+        WHERE
+        username = "${authorUsername}"
+        AND
+        validation = "Y";
+      `);
+      
+      if (authorRow.length === 0) {
+        req.flash('message', {'info' : '존재하지 않는 회원입니다'});
+        return res.redirect('/');
+      }
+      const author = authorRow[0];
+      
+      // profile에 필요한 좋아요, 작성 게시글, 팔로워, 팔로우 수 가져오기
+      const [authorInfoRow] = await pool.query(
+        `
+        SELECT *
+        FROM 
+        (select count(*) like_count from LIKES where user_id = 36) as likes
+        JOIN
+        (select count(*) post_count from POSTS where user_id = 36) as posts
+        JOIN
+        (select count(*) following_count from FOLLOW where follower_id = 36) as following
+        JOIN
+        (select count(*) follower_count from FOLLOW where follower_id = 36) as follower
+        `
+      )
+      author.info = authorInfoRow[0];
 
-    let user;
+      // 해당 profile author가 게시한 게시물 가져오기
+      const [posts] = await pool.query(`
+        SELECT * FROM POSTS WHERE USER_ID = ${author.id} and validation = "Y";
+      `)
 
-    if (req.user) {
-      user = await User.findById(req.user._id);
+      // 접속한 로그인 유저 정보
+      const user = req.user;
+      if (user) {
+
+        // profile author 와 follow 관계인지 확인
+        const [followRow] = await pool.query(`
+          SELECT COUNT(*) as isFollow FROM FOLLOW WHERE follower_id = "${user.id}" and following_id = "${author.id}"
+        `)
+
+        user.isFollow = followRow[0].isFollow;
+
+        // 좋아요를 누른 게시물 가져오기
+        const [likeRow] = await pool.query(`
+          SELECT * FROM LIKES WHERE user_id = ${user.id};
+        `)
+
+        const likeObj = likeRow.reduce((acc, val) => {
+          const post = val.POST_ID;
+          acc[post] = post;
+          return acc;
+        })
+        user.like = likeObj;
+      }
+
+      res.render("profile", {
+        title: "Profile | The creators Network",
+        author,
+        user: req.user,
+        posts
+      });
+
+    } catch (error) {
+      next(error);      
     }
-
-    const posts = author.posts.filter(val => val.display === true);
-    
-    res.render('profile', {
-      title: 'Profile | The creators Network',
-      user: user || null,
-      author,
-      posts,
-      requestUser : req.params.username
-    });
-  },
-}
+  }
+};
 
 module.exports = profileController;
