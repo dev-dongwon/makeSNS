605a3a0bf820b6cb2bae71589fa37b9140b8b453
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-26 02:50:54 +0900

    Modified : isAuthor 미들웨어 mysql로 변환

--- middlewares/content.js
+++ middlewares/content.js
@@ -1,19 +1,25 @@
 const Post = require('../model/post');
+const pool = require('../db/connect-mysql').pool;
 
 const isSameUser = (req, content) => {
-  return req.user.username === content.author.username;
+  return req.user.id === content.USER_ID;
 }
 
 const isAuthor = async (req, res, next) => {
-  
-  const content = await Post.findById(req.params.contentNumber);
+
+  const [contentRow] = await pool.query(`
+    SELECT * FROM POSTS WHERE ID = ${req.params.contentNumber};
+  `)
+
+  const content = contentRow[0];
+
   if (isSameUser(req, content)) {
     req.content = content;
     return next();
   }
 
   req.flash('message', {'warning' : '권한 없음'});
-  return res.redirect(`/contents/${$content._id}`);
+  return res.redirect(`/contents/${content.ID}`);
 };
 
 const isNotAuthor = async (req, res, next) => {
