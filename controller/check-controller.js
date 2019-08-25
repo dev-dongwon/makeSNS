cf7ce9b5a6f18ee96fdf83f2112c90f6616954f8
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-25 19:53:41 +0900

    Modified : id, email 중복 체크 컨트롤러 mysql로 변경

--- controller/check-controller.js
+++ controller/check-controller.js
@@ -1,11 +1,15 @@
-const User = require('../model/user');
+const pool = require('../db/connect-mysql').pool;
 
 const checkController = {
   checkDupleUsername : async (req, res, next) =>{
     try {
       const username = req.params.username;
-      const user = await User.findOne({ username });
-      if (user) {
+      
+      const [userRow] = await pool.query(`
+        SELECT ID FROM USERS WHERE USERNAME = "${username}";
+      `)
+      
+      if (userRow.length > 0) {
         return res.send(true);
       }
       return res.send(null);
@@ -18,8 +22,12 @@ const checkController = {
   checkDupleEmail : async (req, res, next) =>{
     try {
       const email = req.params.email;
-      const user = await User.findOne({ email });
-      if (user) {
+      
+      const [emailRow] = await pool.query(`
+        SELECT ID FROM USERS WHERE EMAIL = "${email}";
+      `)
+      
+      if (emailRow > 0) {
         return res.send(true);
       }
       return res.send(null);
