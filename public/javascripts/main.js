ecc8eac6fef5b3ee2cb7ff4c0dfbfb58b67f2727
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-05 16:25:41 +0900

    Modified : Change layout library
    Isotope에서 mansory로 교체 (렌더링 성능 이슈)

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,12 @@
-new Isotope( '.main-grid', {});
+// new Isotope( '.main-grid', {});
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+  // options
+});
